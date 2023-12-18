1201206bbf9f2aadfd4d99d2422f5a362f19f67b
FrancescoMazzocchi <123@email.it>
2023-12-18 17:23:31 +0100

    clear code

--- alberghi.php
+++ alberghi.php
@@ -78,46 +78,10 @@ foreach ($hotels as $key => $value) {
         <td>'.$parcheggio.'</td>
         <td>'.$value["vote"].'</td>
         <td>'.$value["distance_to_center"].' km</td>
-        
         </tr>';
     }
-    
-
-
 };
 echo '</tbody></teble';
-
-
-
-
-
-    // foreach ($hotels as $hotel) {
-
-    //     echo "Nome: ".$hotel['name'];
-    //     echo '<br>';
-    //     echo "Descrizione: ".$hotel['description'];
-    //     echo '<br>';
-
-    //     $parcheggio='manca';
-
-    //     if ($hotel['parking']==true) {
-    //         $parcheggio='presente';
-    //     }
-    //     echo "Parcheggio: ".$parcheggio;
-    //     echo '<br>';
-
-    //     echo "Voto: ".$hotel['vote'];
-    //     echo '<br>';
-    //     echo "Distanza dal centro: ".$hotel['distance_to_center']." km";
-    //     echo '<br>';
-        
-    //     echo '<br><br>';
-
-
-
-
-    // };
-
 
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
